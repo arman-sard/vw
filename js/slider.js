86cdf89ef0525c9fc238f087a9c40ece08d374a5
arman <arman@sard.am>
2019-10-17 17:51:16 +0400

    Fixed: disable scroll on menu hover

--- js/slider.js
+++ js/slider.js
@@ -13,14 +13,30 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("carousel-slider-img");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
+        //   slides[i].classList.remove('slider-visible');
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    // slides[slideIndex-1].classList.add('slider-visible');
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    //console.log(slideIndex);
+}
+
+function scrollOff() {
+    window.scrollTo(0, 0);
+    window.addEventListener('scroll', scrollOff);
+}
+function scrollOn(){
+    window.removeEventListener('scroll', scrollOff);
 }
